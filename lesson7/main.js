ee3c6c69ed7830e5a37cfbb032aa59a76c7858b7
Ostap Khomitskyi <ostapkhomitskyi@ukr.net>
2016-07-18 17:33:24 +0300

    Update main.js

--- lesson7/main.js
+++ lesson7/main.js
@@ -2,47 +2,49 @@
 
     var prev = document.getElementById('prev');
     var next = document.getElementById('next');
-    var currentHero = {};
+    var heroInfoList = document.getElementById('hero-info');
+    var heroFilmsList = document.getElementById('hero-films');
+    var loadingBlock = document.getElementById('loading');
+    var currentHeroInfo = {};
+    var currentHeroFilms = [];
     var index = 1;
 
     document.addEventListener("DOMContentLoaded", () => {
-
         getHero(index);
 
-    });
+        prev.addEventListener('click', e => {
+            if (index <= 1) {
+                prev.classList.add('disabled');
 
-    prev.addEventListener('click', e => {
-        if (index <= 1) {
-            prev.classList.add('disabled');
+                return false;
+            }
+            if (index === 2) {
+                prev.classList.add('disabled');
+            }
 
-            return false;
-        }
-        if (index === 2) {
-            prev.classList.add('disabled');
-        }
+            getHero(--index);
 
-        getHero(--index);
+            if (index === 87) {
+                next.classList.remove('disabled');
+            }
+        });
 
-        if (index === 87) {
-            next.classList.remove('disabled');
-        }
-    });
+        next.addEventListener('click', e => {
+            if (index >= 88) {
+                next.classList.add('disabled');
 
-    next.addEventListener('click', e => {
-        if (index >= 88) {
-            next.classList.add('disabled');
+                return false;
+            }
+            if (index === 87) {
+                next.classList.add('disabled');
+            }
 
-            return false;
-        }
-        if (index === 87) {
-            next.classList.add('disabled');
-        }
+            getHero(++index);
 
-        getHero(++index);
-
-        if (index === 2) {
-            prev.classList.remove('disabled');
-        }
+            if (index === 2) {
+                prev.classList.remove('disabled');
+            }
+        });
     });
 
     function status(response) {
@@ -70,51 +72,59 @@
         return str.slice(0, 1).toUpperCase() + str.slice(1).split('_').join(' ');
     }
 
-    function renderHero(hero) {
-        var heroInfo = document.getElementById('hero-info');
-        var heroFilms = document.getElementById('hero-films');
+    function renderHeroInfo(hero) {
         var fragment = document.createDocumentFragment();
 
-        heroInfo.innerHTML = '';
         for (var prop in hero) {
             var li = document.createElement('li');
-
             li.textContent = toNormalCase(prop) + ': ' + hero[prop];
             fragment.appendChild(li);
         }
 
-        heroInfo.appendChild(fragment);
+        heroInfoList.appendChild(fragment);
+    }
+
+    function renderHeroFilms(films) {
+        var fragment = document.createDocumentFragment();
+        films = films.sort((a, b) => a.id - b.id);
+
+        films.map(film => {
+            var li = document.createElement('li');
+            var liContent = `Episode ${film.id}: ${film.title}`;
+            li.textContent = toNormalCase(liContent);
+            fragment.appendChild(li);
+        });
+
+        heroFilmsList.appendChild(fragment);
     }
 
     function getHero(index) {
-
+        heroInfoList.innerHTML = '';
+        heroFilmsList.innerHTML = '';
+        loadingBlock.innerHTML = '<h3 class="loading-text">Loading...</h3>';
+        
         fetch('http://swapi.co/api/people/' + index)
             .then(status)
             .then(json)
             .then(data => {
-                copySomeProps(currentHero, data, 8);
+                copySomeProps(currentHeroInfo, data, 8);
 
-                // fetch(data.films[0])
-                //     .then(status)
-                //     .then(json)
-                //     .then(data => console.log(data));
-
-                // return Promise.all( data.films.map((url) => {
-                //    return fetch(url);
-                // }));
+                return Promise.all( data.films.map(url => fetch(url)));
             })
-            // .then(status)
-            // .then(json)
-            // .then(films => {
-            //     films.forEach((film) => {
-            //         currentHero.filmTitles = [];
-            //         currentHero.filmTitles.push(film);
-            //     });
-            // })
+            .then(responses => Promise.all(responses.map(response => response.json())))
+            .then(films => {
+                currentHeroFilms.length = 0;
+                films.forEach(film => {
+                    currentHeroFilms.push({
+                        title: film.title,
+                        id: film.episode_id
+                    });
+                });
+            })
             .then(() => {
-                document.getElementById('hero-info').innerHTML = '<h2>Loading info...</h2>';
-
-                renderHero(currentHero);
+                loadingBlock.innerHTML = '';
+                renderHeroInfo(currentHeroInfo);
+                renderHeroFilms(currentHeroFilms);
             })
             .catch(err => console.log('Something wrong with fetch...', err));
     }
